cdaaf983a90bc4b9929309f8a8667ed9e5f08418
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-03-19 17:31:24 +0530

    Add files via upload
    This program is used to calculate the percentage of 5 subjects of a student after taking input from the user.

---
+++ Percentage.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Percent
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter 5 subject marks");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter 1st subject marks ");
+        int a = s.nextInt();
+        System.out.println("Enter 2nd subject marks");
+        int b = s.nextInt();
+        System.out.println("Enter 3rd subject marks");
+        int c = s.nextInt();
+        System.out.println("Enter 4th subject marks");
+        int d = s.nextInt();
+        System.out.println("Enter 5th subject marks");
+        int e = s.nextInt();
+        float x = (a+b+c+d+d+e)/5;
+        System.out.println("Percentage is " + x);
+
+    }
+}
