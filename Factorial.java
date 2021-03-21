eebda6ec3534a82290d3be79dd7d63d9d74c068f
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-03-21 13:57:07 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        int fact = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            fact = fact*i;
+        }
+        System.out.println(fact) ;
+    }
+}
