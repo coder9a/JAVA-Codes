66584f80a8ae81134204c83906f013860a0be595
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-03-25 22:40:22 +0530

    Add files via upload

---
+++ Prime_Number_Sum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Prime_Number_Sum
+{
+    public static void main(String[] args)
+    {
+        int count = 0,sum=0,num,j;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter upper limit ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter lower limit ");
+        int n2 = sc.nextInt();
+            for(num=n1;num<=n2;num++)
+            {
+                count = 0;
+                for (j = 2; j <= n1/2; j++)
+                {
+                    if (num%j == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if(count ==0 && num != 1)
+                {
+                    sum=sum+num;
+                    System.out.println(num+" ");
+                }
+            }
+            System.out.println("Sum is : "+sum);
+    }
+}
+
